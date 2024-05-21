5e85461b7c6986a73cb0f1badf8d8b8886717b3f
eulerpb <eulerpablo10@gmail.com>
2024-05-20 21:37:19 -0300

    Integrando Firebase ao projeto

--- js/main.js
+++ js/main.js
@@ -139,3 +139,20 @@ function showSlides() {
 document.addEventListener('DOMContentLoaded', showSlides);
 
 
+/* Importação do firebase */
+
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBnZ5hjZkaSYWCyFTFlX8oj0R2oTmoG8JQ",
+  authDomain: "site-casa-azul.firebaseapp.com",
+  projectId: "site-casa-azul",
+  storageBucket: "site-casa-azul.appspot.com",
+  messagingSenderId: "767221469467",
+  appId: "1:767221469467:web:adba4b662835c9188143be"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
