1f9550baa0b970ce5b292b6b6828ce84df06067f
Gabriel <carloscgmorais@gmail.com>
2024-05-21 11:10:31 -0300

    correção de bug no login

--- login/index.js
+++ login/index.js
@@ -1,12 +1,4 @@
-function onChangeEmail() {
-  toggleButtonsDisable();
-  toggleEmailErrors();
-}
 
-function onChangePassword() {
-  toggleButtonsDisable();
-  togglePasswordErrors();
-}
 
 function login() {
   firebase.auth().signInWithEmailAndPassword(
@@ -29,25 +21,7 @@ function register() {
   window.location.href = "pages/register/register.html";
 }
 
-function toggleEmailErrors() {
-  const email = form.email().value;
-  form.emailRequiredError().style.display = email ? "none" : "block";
-  
-  form.emailInvalidError().style.display = validateEmail(email) ? "none" : "block";
-}
 
-function togglePasswordErrors() {
-  const password = form.password().value;
-  form.passwordRequiredError().style.display = password ? "none" : "block";
-}
-
-function toggleButtonsDisable() {
-  const emailValid = isEmailValid();
-  form.recoverPasswordButton().disabled = !emailValid;
-
-  const passwordValid = isPasswordValid();
-  form.loginButton().disabled = !emailValid || !passwordValid;
-}
 
 function isEmailValid() {
   const email = form.email().value;
