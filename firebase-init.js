4e7660e92617f90f9ffa4f17638534481553994c
Gabriel <carloscgmorais@gmail.com>
2024-05-21 11:22:29 -0300

    adequação de iniciação do firebase

---
+++ firebase-init.js
@@ -0,0 +1,11 @@
+    const firebaseConfig = {
+  apiKey: "AIzaSyBnZ5hjZkaSYWCyFTFlX8oj0R2oTmoG8JQ",
+  authDomain: "site-casa-azul.firebaseapp.com",
+  projectId: "site-casa-azul",
+  storageBucket: "site-casa-azul.appspot.com",
+  messagingSenderId: "767221469467",
+  appId: "1:767221469467:web:adba4b662835c9188143be"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
